0ef9de3841075ac13fc4e2e0fa317f3e037360df
Madhu HS <madhu.s.ty@cars24.com>
2025-08-09 16:03:52 +0530

    added hibernate crud ops

--- HibernateDemo/src/main/java/com/jspiders/hibernate/Main.java
+++ HibernateDemo/src/main/java/com/jspiders/hibernate/Main.java
@@ -1,11 +1,53 @@
 package com.jspiders.hibernate;
 
+import com.jspiders.hibernate.entities.ContactEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 public class Main {
+
+    private static final String hibernateCfgFile = "hibernate.cfg.xml";
+
     public static void main(String[] args) {
         System.out.println("Starts...");
 
-        Session
+        //Load configuration
+        Configuration configuration = new Configuration();
+        configuration.configure(hibernateCfgFile);
+
+        //Create session (Connection)
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+       ContactEntity contactEntity = new
+              ContactEntity(0,"A","999999999","test@gmail.com");
+
+       //save
+       session.persist(contactEntity);
+
+        //Read
+        ContactEntity contactEntity1 = session.find(ContactEntity.class,1);
+        System.out.println(contactEntity1.getId());
+        System.out.println(contactEntity1.getName());
+        System.out.println(contactEntity1.getEmail());
+        System.out.println(contactEntity1.getPhone());
+
+
+        //update
+        ContactEntity contactEntity2 = session.find(ContactEntity.class,2);
+        contactEntity2.setEmail("newEmail2@gmail.com");
+
+        //delete
+        ContactEntity contactEntity3 = session.find(ContactEntity.class,3);
+        session.remove(contactEntity3);
+
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
 
         System.out.println("ends...");
     }
---
+++ HibernateDemo/src/main/java/com/jspiders/hibernate/entities/ContactEntity.java
@@ -0,0 +1,57 @@
+package com.jspiders.hibernate.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contact")
+public class ContactEntity {
+    public ContactEntity(int id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+    public ContactEntity(){
+
+    }
+
+    @Id
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
