81115a57d5ad2e5eb7ec8df8126a3ccb0980ef44
Madhu HS <madhu.s.ty@cars24.com>
2025-08-09 15:04:18 +0530

    added hibernate cfg file

---
+++ HibernateDemo/src/main/java/com/jspiders/hibernate/Main.java
@@ -0,0 +1,12 @@
+package com.jspiders.hibernate;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starts...");
+
+        Session
+
+        System.out.println("ends...");
+    }
+}
