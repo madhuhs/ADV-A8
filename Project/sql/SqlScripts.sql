5eb9179328bd3467cc3046577ee2b8b60223346f
Madhu HS <madhu.s.ty@cars24.com>
2025-10-08 14:52:58 +0530

    project : added sql scripts

---
+++ Project/sql/SqlScripts.sql
@@ -0,0 +1,9 @@
+SELECT * FROM hms.hotel_info;
+SELECT * FROM hms.hotel_contact_info;
+
+insert into hms.hotel_info values(
+                                     1,'Hotel1','hotel','p1',4.0,'t1',null);
+
+insert into hms.hotel_contact_info values(
+                                             1,'add1','city1','c1','india',1,'hotel@gmail.com','fax1','909090901','8981','1',1
+                                         );
